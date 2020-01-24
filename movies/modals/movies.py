bf311c849ae8baf96d95e76511be6d170bb35727
Moksleivis <moksleivis@KITM.local>
2020-01-24 11:04:27 +0200

    first commit

--- movies/modals/movies.py
+++ movies/modals/movies.py
@@ -4,10 +4,10 @@ from movies.database import create_table_database
 def create_movies_table():
     query = """CREATE TABLE IF NOT EXISTS movie (
                         movie_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        movie_name
-                        release_date
-                        rating
-                        box_office_name
+                        movie_name TEXT UNIQUE,
+                        release_date TEXT,
+                        rating TEXT,
+                        box_office_name TEXT,
                         studio_name TEXT)"""
     create_table_database(query)
 
