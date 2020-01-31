afc1feda143f0c64f355307d0666476dbd1556e4
Vytautas <vytautasragaisis07@gmail.com>
2020-01-31 11:21:41 +0200

    first commit

--- database/modals/box_offices.py
+++ database/modals/box_offices.py
@@ -1,4 +1,4 @@
-from database.database import create_table_database, insert_query
+from database.database import create_table_database, insert_query, get_database
 from entities.box_office import  box_office
 
 def create_box_offices_table():
@@ -7,18 +7,18 @@ def create_box_offices_table():
                         box_office_gross TEXT)"""
     create_table_database(query)
 
-create_box_offices_table()
 
 box_office1 = box_office(None, "15000")
 
 def create_box_office(box_office):
-    query = """INSERT INTO box_offices VALUES (?, ?)"""
+    query = "INSERT INTO box_offices VALUES (?, ?)"
     params = (box_office.box_office_id, box_office.box_office_gross)
     insert_query(query, params)
 
 def get_box_office():
     query = "SELECT * FROM box_offices WHERE box_office_id = (?) OR box_office_gross = (?)"
-    insert_query(query)
+    params = (box_office.box_office_id, box_office.box_office_gross)
+    get_database(query, params)
 
 def update_box_office(box_office):
     query = "UPDATE box_offices SET box_office_gross = (?) WHERE box_office_id = (?)"
